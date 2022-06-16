d962812d89ed94ac5b6d6d513aeb5dea282ab9be
Ludovic Muller <ludovicmuller1@gmail.com>
2022-06-16 11:50:57 +0200

    upgrade dependencies

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { createWrapper } from "next-redux-wrapper";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./rootReducer";
